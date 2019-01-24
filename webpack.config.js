e85a522a3dc54fa5d5aacc50e4dfd787c1cb147d
evgenyoborotistov <evgeny.oborotistov@weare.fi>
2019-01-24 12:25:34 +0200

    Initial webpack 4 and babel 7 setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const presets = require("./babel.config");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [presets]
+          }
+        }
+      }
+    ]
+  }
+};
